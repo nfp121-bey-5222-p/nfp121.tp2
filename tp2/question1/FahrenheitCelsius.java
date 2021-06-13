ceb601aac38a67882b1fd78151222fb83bfc3ddd
claireelamil <claire.el-amil@isae.edu.lb>
2021-06-13 20:11:45 +0300

    adding testq1

--- tp2/question1/FahrenheitCelsius.java
+++ tp2/question1/FahrenheitCelsius.java
@@ -2,6 +2,7 @@ package question1;
 public class FahrenheitCelsius {
     public static void main(String[] args) {
         int fahrenheit=0;
+    
         float celsius=0;
     
         System.out.println(fahrenheit + "\u00B0F -> " + celsius + "\u00B0C"); // ligne,
